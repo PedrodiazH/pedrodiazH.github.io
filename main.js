c3f164f75b2a04a1fae57b49664678e2fa8608b4
PedrodiazH <pedroignaciodh@gmail.com>
2023-09-10 17:02:43 -0300

    Pag 1

---
+++ main.js
@@ -0,0 +1,302 @@
+const jsonData = [
+    {
+     "palabra": "INEXORABLE",
+     "definicion": "Que no se puede evitar, eludir o detener.",
+     "numero": 1,
+     "uso": "El inexorable paso del tiempo."
+    },
+    {
+        "palabra": "INVERECUNDO, INVERECUNDA",
+        "definicion": "Desvergonzado, que no tiene vergüenza.",
+        "numero": 2
+       },
+    {
+     "palabra": "INMARCESIBLE",
+     "definicion": "Que no se puede marchitar, inmarchitable.",
+     "numero": 3,
+     "uso": "Un bonito recuerdo es inmarcesible."
+    },
+    {
+     "palabra": "JIBARIZAR",
+     "definicion": "Reducir el tamaño, la cantidad o la importancia de una cosa.",
+     "numero": 4,
+     "uso": "El gobierno jibarizó las subvenciones públicas a diversos museos"
+    },
+    {
+        "palabra": "SICOFANTE, SICOFANTA",
+        "definicion": "Impostor, calumniador. Busca obtener una posición o estatus personal mediante adulación hacia otras personas que comúnmente disponen ya de ciertas influencias y estatus social.",
+        "numero": 5
+       },
+    {
+     "palabra": "BALDRAGAS",
+     "definicion": "Hombre débil, sin fuerza física ni energía.",
+     "numero": 6,
+     "uso": "Te comportas como un baldragas al no hacer nada por tu vida"
+    },
+    {
+     "palabra": "DROMOMANÍA",
+     "definicion": "Inclinación excesiva u obsesión patológica por trasladarse de un lugar a otro.",
+     "numero": 7
+    },
+    {
+     "palabra": "INASIBLE",
+     "definicion": "Que no se puede tomar con la mano.",
+     "numero": 8,
+     "uso": "El alma y la conciencia es inasible"
+    },
+    {
+     "palabra": "PERENNE",
+     "definicion": "Continuo, incesante, que no tiene intermisión.",
+     "numero": 9,
+     "uso": "La pobreza es un problema perenne en América Latina"
+    },
+    {
+     "palabra": "VITUPERIO",
+     "definicion": "Censura o desaprobación que una persona hace con mucha dureza contra algo o alguien.",
+     "numero": 10,
+     "uso": "el público lanzaba vituperios contra el árbitro por su falta de imparcialidad."
+    },
+    {
+     "palabra": "COLEGIR",
+     "definicion": "Sacar una conclusión por medio de un razonamiento a partir de hechos, indicios, supuestos anteriores o de un principio general.",
+     "numero": 11,
+     "uso": "Era imposible colegir cuales eran las reales intenciones del gobernante."
+    },
+    {
+     "palabra": "ATIZAR",
+     "definicion": "1. Remover o alimentar el fuego para que arda más.\n\n2. Hacer más fuerte o intenso un sentimiento o una pasión.",
+     "numero": 12,
+     "uso": "La nueva situación política atizó el terror entre los dirigentes del partido"
+    },
+    {
+     "palabra": "ENCOMIAR",
+     "definicion": "Alabar con encarecimiento a alguien o algo.",
+     "numero": 13,
+     "uso": "Quisiera manifestarle mi agradecimiento y encomiar sus incansables esfuerzos."
+    },
+    {
+     "palabra": "EXÓGENO",
+     "definicion": "De origen externo.",
+     "numero": 14,
+     "uso": "Un sequía podría ser un evento exógeno que impacte en la producción de algún producto agrícola."
+    },
+    {
+     "palabra": "PULULAR",
+     "definicion": "Abundar y moverse mucho en un lugar [personas, animales o cosas].",
+     "numero": 15,
+     "uso": "Las particulas elementales pululan por el universo."
+    },
+    {
+     "palabra": "INEFABLE",
+     "definicion": "Que no puede ser dicho, explicado o descrito con palabras, generalmente por tener cualidades excelsas o por ser muy sutil o difuso.",
+     "numero": 16,
+     "uso": "El siente un orgullo inefable por los triunfos de sus hijos, es algo típico de todos los padres, esto es así."
+    },
+    {
+     "palabra": "AFLAUTADO, AFLAUTADA",
+     "definicion": "Sonido agudo y suave, como el de la flauta.",
+     "numero": 17,
+     "uso": "Ella se reía de una manera muy aflautada."
+    },
+    {
+     "palabra": "VELEIDOSO, VELEIDOSA",
+     "definicion": "Que es poco constante en sus actos, gustos o ideas.",
+     "numero": 18
+    },
+    {
+     "palabra": "MUDABLE",
+     "definicion": "Persona que muda con facilidad de opinión, gustos, etc.",
+     "numero": 19,
+     "uso": "Mi mejor amiga es muy mudable en cuanto a sus inquietudes."
+    },
+    {
+     "palabra": "CULTIPICAÑO",
+     "definicion": "Persona culta y picaresca conjuntamente.",
+     "numero": 20,
+     "uso": "Tu simpatía y manejo de distintos temas te hacen un cultipicaño."
+    },
+    {
+     "palabra": "RIMBOMBANTE",
+     "definicion": "Ostentoso, llamativo.",
+     "numero": 21,
+     "uso": "El cantante se hizo popular sin escándalos ni frases rimbombantes"
+    },
+    {
+     "palabra": "ESTÓLIDO",
+     "definicion": "Falto de razón y discurso.",
+     "numero": 22,
+     "uso": "No tuvo más respuesta que un silencio estólido."
+    },
+    {
+     "palabra": "INOBJETABLE",
+     "definicion": "Que no puede ser objetado o recibir objeciones.",
+     "numero": 23,
+     "uso": "Cuando es un hecho científico correctamente observado o una ley correctamente establecida, ese saber expresa una verdad inobjetable."
+    },
+    {
+     "palabra": "FLÉBIL ",
+     "definicion": "1. Digno de ser llorado.\n\n2. Lamentable, triste, lacrimoso.",
+     "numero": 24,
+     "uso": "La muerte de un gran amigo siempre es una situación flébil."
+    },
+    {
+     "palabra": "SÓRDIDO",
+     "definicion": "1. Que es miserable o sucio\n\n2. Que es indecente o malicioso.",
+     "numero": 25,
+     "uso": "1. Aquella persona vivía en una choza sórdida. \n\n2. Aquel estafador tenía intenciones sórdidas."
+    },
+    {
+     "palabra": "DELEZNABLE ",
+     "definicion": "Que es despreciable y no merece ser tenido en consideración.",
+     "numero": 26
+    },
+    {
+     "palabra": "CHABACANO",
+     "definicion": "Que es grosero, ordinario o de mal gusto.",
+     "numero": 27
+    },
+    {
+     "palabra": "SUSPICAZ",
+     "definicion": "Propenso a concebir sospechas o a tener desconfianza.",
+     "numero": 28
+    },
+    {
+     "palabra": "MONOMANÍA",
+     "definicion": "Locura o delirio parcial sobre una sola idea o un solo orden de ideas.",
+     "numero": 29
+    },
+    {
+     "palabra": "ARDID",
+     "definicion": "Acción hábil con que se pretende engañar a alguien o conseguir algo.",
+     "numero": 30
+    },
+    {
+     "palabra": "ALIPORI",
+     "definicion": "Verguenza ajena. Concepto oficial de lo coloquialmente llamando \"cringe\"",
+     "numero": 31
+    },
+    {
+     "palabra": "INTRINCAR",
+     "definicion": "1. Enredar o enmarañar una cosa.\n\n2. Confundir los pensamientos o conceptos.",
+     "numero": 32
+    },
+    {
+     "palabra": "FASTUOSO",
+     "definicion": "Que causa admiración por su grandeza, lujo o perfección.",
+     "numero": 33
+    },
+    {
+     "palabra": "FATUO ",
+     "definicion": "[persona] Que se muestra engreído en su actitud, comportamiento o manera de hablar.",
+     "numero": 34
+    },
+    {
+     "palabra": "MISÁNTROPO",
+     "definicion": "Persona que huye del trato con otras personas, sientiendo gran aversión o rechazo hacia ellas.",
+     "numero": 35
+    },
+    {
+        "palabra": "INCONMESURABLE",
+        "definicion": "Enorme, que por su gran magnitud no puede medirse.",
+        "numero": 36,
+        "uso": "Tengo una colección de un valor sentimental inconmesurable."
+       },
+    {
+     "palabra": "ESTRÉPITO",
+     "definicion": "1. Ruido considerable.\n\n2.Ostentación, aparato en la realización de algo.",
+     "numero": 37
+    },
+    {
+     "palabra": "TORPEDEAR",
+     "definicion": "Hacer fracasar un asunto o un proyecto.",
+     "numero": 38
+    },
+    {
+     "palabra": "JERARCA",
+     "definicion": "Persona que tiene elevada categoría en una organización, una empresa, etc.",
+     "numero": 39
+    },
+    {
+     "palabra": "ESOTÉRICO",
+     "definicion": "1. Oculto, reservado.\n\n2. Dicho de una cosa: Que es impenetrable o de difícil acceso para la mente.",
+     "numero": 40
+    },
+    {
+     "palabra": "EXOTÉRICO",
+     "definicion": "Común, accesible para el vulgo, en oposición a esotérico.",
+     "numero": 41
+    },
+    {
+     "palabra": "BRAVATA",
+     "definicion": "Amenaza proferida con arrogancia para intimidar a alguien.",
+     "numero": 42
+    },
+    {
+     "palabra": "LABRIEGO,LABRIEGA",
+     "definicion": "Persona que tiene por oficio trabajar y cultivar la tierra y vive en el medio rural.",
+     "numero": 43
+    },
+    {
+     "palabra": "SOCARRONERÍA",
+     "definicion": "Modo de expresarse de la persona que se burla de los demás de manera irónica y con apariencia de ingenuidad.",
+     "numero": 44
+    },
+    {
+        "palabra": "AUTOPIESIS",
+        "definicion": "Concepto asociado a que todo ser vivo se encuentra dentro de un sistema cerrado que constantemente está creciendo y creándose a si mismo. Es propuesto por el científico chileno Humberto Maturana.",
+        "numero": 45,
+        "uso": "Ejemplo de uso: Un bonito recuerdo es inmarcesible."
+       },
+    {
+     "palabra": "GRISÁCEO",
+     "definicion": "Que tiene un tono gris.",
+     "numero": 46
+    },
+    {
+     "palabra": "ESCUETO",
+     "definicion": "Que es simple, sin adornos, detalles superfluos ni cosas innecesarias.",
+     "numero": 47
+    },
+    {
+     "palabra": "ABSTRUSO",
+     "definicion": "De difícil comprensión.",
+     "numero": 48
+    },
+    {
+     "palabra": "ECUÁNIME",
+     "definicion": "Que obra con rectitud, justicia e imparcialidad o está dotado de ellas",
+     "numero": 49
+    },
+    {
+     "palabra": "AMAINAR",
+     "definicion": "1. Dicho del viento: Aflojar, perder su fuerza.\n\n2. Aflojar o ceder en algún deseo, empeño o pasión.",
+     "numero": 50
+    }
+   ]; 
+// Hasta aca llega el JSON
+
+/* Esta parte es para actualizar contenido diariamente del html,
+en dirección al sitio web*/
+let indicePalabraActual = 0;
+const palabraElement = document.getElementById("palabra");
+const definicionElement = document.getElementById("definicion");
+const usoElement = document.getElementById("uso");
+
+function cambiarPalabra(){
+    palabraElement.textContent = jsonData[indicePalabraActual].palabra;
+    definicionElement.textContent = jsonData[indicePalabraActual].definicion;
+    usoElement.textContent = jsonData[indicePalabraActual].uso;
+
+    indicePalabraActual = (indicePalabraActual + 1) % jsonData.length
+    /* es como un ciclo en donde el incremento es +1, de esta forma la palabra cambia
+    mediante el numero q se le asigna, %jsonData.length es para q al acabar el ciclo en
+    el tamaño del json, este vuelva al comienzo. */
+}
+/*  esto garantiza que las palabras se muestren en el orden 
+correspondiente a sus números en el JSON */
+jsonData.sort((a, b) => a.numero - b.numero);
+
+cambiarPalabra(); // llamar función
+setInterval(cambiarPalabra, 5000); // prueba cada 5 segundos
+// setInterval(cambioPalabra, 24 * 60 * 60 * 1000) // Cada 24 horas hay cambio
+// Conversión 24 horas a milisegundos  = (24 * 60 * 60 * 1000)
